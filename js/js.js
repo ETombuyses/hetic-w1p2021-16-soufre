eca99b583f3aad9b265bf3ac5f7c51b2dcaa92cf
nekocheik <44557532+nekocheik@users.noreply.github.com>
2018-11-14 21:47:36 +0100

    Update js/js.js

--- js/js.js
+++ js/js.js
@@ -6,19 +6,11 @@ open.addEventListener('click', function() {
 
 
 
-var accueil = document.getElementById('navaccueil');
-var montres = document.getElementById('navmontres');
-var marque = document.getElementById('navmarque');
-
+var accueil = document.getElementById('navbar');
 accueil.addEventListener('click', function() {
-  nav.classList.toggle('is-open');
-});
-montres.addEventListener('click', function() {
-  nav.classList.toggle('is-open');
-});
-marque.addEventListener('click', function() {
-  nav.classList.remove('is-open');
-});
+nav.classList.toggle('is-open');
+}); 
+
 
 
 
