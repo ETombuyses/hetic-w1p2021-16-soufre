8a005be4004c96268d62ef90e3b546c0b3c8037b
HETIC <hetic@MacBook-Pro-de-Joan.local>
2018-11-14 13:09:25 +0100

    js

--- js/js.js
+++ js/js.js
@@ -3,3 +3,18 @@ var open = document.getElementById('open')
 open.addEventListener('click', function() {
   nav.classList.toggle('is-open');
 });
+
+
+var imgsfront = document.querySelectorAll('image-front');
+var imgsback = document.querySelectorAll('image-back');
+
+for (var count=0; count<imgsfront.length; count++) {
+  let imgfront = imgsfront[count];
+  let imgback = imgsback[count];
+
+imgfront.addEventListener('mouseover', function () {
+
+imgfront.classList.remove()
+});
+
+}
