a2f36f3de2aa964ebba54fb4af0bcc18f3d0e1f7
HETIC <hetic@MacBook-Pro-de-Joan.local>
2018-11-15 14:41:23 +0100

    medias

--- js/product.js
+++ js/product.js
@@ -1,24 +1,24 @@
 /* JS PRODUIT */
 
 
-  var button1 = document.getElementById('btnColors');
-  var colors = document.getElementById('watch-colors');
-  button1.onclick = function(e){
-      if(colors.style.display=="flex"){
-          colors.style.display="none";
-      }else{
-          colors.style.display="flex";
-      }
-  };
+var button1 = document.getElementById('btnColors');
+var colors = document.getElementById('watch-colors');
+button1.onclick = function(e){
+if(colors.style.display=="flex"){
+colors.style.display="none";
+}else{
+colors.style.display="flex";
+}
+};
 
 var button2 = document.getElementById('btnDetails');
 var details = document.getElementById('detailsList');
 
 button2.onclick = function(e){
-  if(details.style.display=="block"){
-  details.style.display="none";
-  }
-  else {
-  details.style.display="block";
-  }
-    };
+if(details.style.display=="block"){
+details.style.display="none";
+}
+else {
+details.style.display="block";
+}
+};
